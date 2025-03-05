d4abde1e7d5d875635708af31ffc471e43a070ca
Rohan <shingaderohan96@gmail.com>
2025-03-05 13:29:54 +0530

    comments and unnecessary code removed

--- spinner-next-app/src/pages/index.tsx
+++ spinner-next-app/src/pages/index.tsx
@@ -1,16 +1,8 @@
-import Head from "next/head";
 import LoadingSpinner from "../components/LoadingSpinner";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Loading Spinner</title>
-        <meta name="description" content="A simple loading spinner in Next.js" />
-      </Head>
-      {/* <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-        <LoadingSpinner color="#0ca2de" size="large" text="EC"/>
-      </div> */}
+    <>  
       <div className="flex justify-center items-center h-screen">
         <LoadingSpinner color="#0ca2de" size="large" text="EC"/>
       </div>
